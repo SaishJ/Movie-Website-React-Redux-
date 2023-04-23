90b8e197a69f4d63ac7165871cde9fb5619e3caa
SaishJ <saish.jagtap@dwebbox.com>
2023-04-23 16:30:08 +0530

    Make API call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
 
 const Home = () => {
-  return <div>Home</div>;
+  useEffect(() => {
+    const movieText = "Avatar";
+    const fetchMovies = async () => {
+      const response = await movieApi
+        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.error("Error", err);
+        });
+      console.log("Response from API", response);
+    };
+
+    fetchMovies();
+  }, []);
+  return (
+    <div>
+      <div className="banner-image"></div>
+      <MovieListing />
+    </div>
+  );
 };
 
 export default Home;
