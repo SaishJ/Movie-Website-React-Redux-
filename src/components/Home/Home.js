d5f149f212b80090d15c4994765194bfcdfa0f53
SaishJ <saish.jagtap@dwebbox.com>
2023-04-26 00:34:58 +0530

    Setting up Redux

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,20 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../redux/movies/movieSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = "Avatar";
   useEffect(() => {
-    const movieText = "Avatar";
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.error("Error", err);
         });
-      console.log("Response from API", response);
+      dispatch(addMovies(response.data));
     };
 
     fetchMovies();
